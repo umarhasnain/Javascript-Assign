162e3c5f3eb1fba1bc8169400866b2fa0198b9a5
Umar Hasnain <103918550+umarhasnain@users.noreply.github.com>
2023-04-06 21:40:12 +0500

    Add files via upload

---
+++ Assign#01/app.js
@@ -0,0 +1,120 @@
+////********************************* Task 1 ***********************************
+
+// let user1 = +prompt("Enter Your First Number");
+// let user2 = +prompt("Enter Your Second Number");
+    
+// if(user1 > user2){
+//     console.log("The Larger Number is: "+ user1)
+// }else if(user2 > user1){
+//     console.log("The Larger Number is: "+user2)
+// }else{
+//     console.log("The Number are Equal ")
+
+// }
+
+
+//********************************* Task 2 *************************************
+
+// let user = +prompt("Enter Some Number");
+// if(user > 0){
+//     alert("Sign is Positive (+) "+ user)
+// }
+// else if(user < 0){
+//     alert("Sign is Negative (-) "+ user)
+// }else{
+//     alert("Number is Zero")
+// }
+
+//********************************* Task 3 ****************************
+
+// let userNum1 = +prompt("Enter Your Number1");
+// let userNum2 = +prompt("Enter Your Number2");
+// let userNum3 = +prompt("Enter Your Number3");
+// let userNum4 = +prompt("Enter Your Number4");
+// let userNum5 = +prompt("Enter Your Number5");
+
+
+// let largeNum = userNum1;
+
+// if (userNum2 > largeNum) {
+//   largeNum = userNum2;
+// }
+
+// else if (userNum3 > largeNum) {
+//   largeNum = userNum3;
+// }
+
+// else if (userNum4 > largeNum) {
+//   largeNum = userNum4;
+// }
+
+// else if (userNum5 > largeNum) {
+//   largeNum = userNum5;
+// }
+// else{
+//     console.log("SomeThing Wrong")
+// }
+
+// console.log("The largest number is: " + largeNum);
+
+
+
+//********************************* Task 4 ****************************
+
+// for(let i = 0; i < 15; i++ ){
+//     if(i%2 ===0) {
+//         console.log(i+ " is Even")
+// } else{
+//     console.log(i + " is Odd")
+// }
+// }
+
+
+
+//********************************* Task 5 ****************************
+
+// let percentage = +prompt("Enter Your Percentage");
+// if (percentage >= 90){
+//     alert("Congratulation! Your Grade is A")
+// }
+// else if(percentage >= 80){
+//   alert("Congratulation! Your Grade is B")
+// }
+// else if(percentage >= 70){
+//   alert("Must Work Hard! Your Grade is C")
+// }
+// else if(percentage >= 60){
+//   alert("Must Work Hard! Your Grade is D")
+// }
+// else if(percentage >= 50){
+//   alert("Improve Your Self!  Your Grade is E")
+// }
+// else{
+//   alert("OOPS! You are Fail")
+// }
+
+
+
+//********************************* Task 6 ****************************
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//       console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//       console.log("Buzz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+  
+//********************************* Task 7 ****************************
+
+// for (let i = 1; i <= 5; i++) {
+//     let row = '';
+//     for (let j = 1; j <= i; j++) {
+//       row += '*';
+//     }
+//     console.log(row);
+//   }
